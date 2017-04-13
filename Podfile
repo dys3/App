e18ffcd653a98a6b20f71f1f052455ebbf7af7d5
Richard Du <richarddu@Richards-Mac.local>
2017-04-12 18:54:44 -0700

    Created new Xcode project with correct app name

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'WhereYouAt' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for App
+    pod 'GooglePlaces'
+    pod 'GooglePlacePicker'
+    pod 'GoogleMaps'
+    pod 'AFNetworking'
+end
---
+++ WhereYouAt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhereYouAt
+//
+//  Created by Richard Du on 4/12/17.
+//  Copyright © 2017 dys3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
