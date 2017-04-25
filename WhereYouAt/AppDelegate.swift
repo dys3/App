bb9f8b31178957b7829b9ae69a1603627fe919b8
Richard Du <rd.richard.du@gmail.com>
2017-04-24 21:36:07 -0700

    Added Views

--- WhereYouAt/AppDelegate.swift
+++ WhereYouAt/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let storyboard = UIStoryboard(name: "Profile", bundle: nil)
+        let vc = storyboard.instantiateInitialViewController()
+        
+        self.window?.rootViewController = vc
+        
         return true
     }
 
