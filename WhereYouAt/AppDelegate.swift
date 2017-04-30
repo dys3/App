4464f90e6c6fa4bb63ad93c6c463e7d010223286
Richard Du <rd.richard.du@gmail.com>
2017-04-30 11:23:32 -0700

    Added new user creation

--- WhereYouAt/AppDelegate.swift
+++ WhereYouAt/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-
+        
+        Parse.initialize(with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) in
+            configuration.applicationId = "wya_dys"
+            configuration.server = "https://wya.herokuapp.com/parse"
+        }))
+        
         return true
     }
 
--- WhereYouAt/NewUserViewController.swift
+++ WhereYouAt/NewUserViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import Parse
 
 class NewUserViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var confirmEmailTextField: UITextField!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var profileNameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +32,43 @@ class NewUserViewController: UIViewController {
     }
     
 
+    @IBAction func onSignup(_ sender: AnyObject) {
+        
+        let alertController = UIAlertController(title: "Error", message: "Username is already taken.", preferredStyle: .alert)
+        
+        // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            // handle response here.
+        }
+        // add the OK action to the alert controller
+        alertController.addAction(OKAction)
+        
+        
+        let newUser = PFUser()
+        
+        newUser.username = usernameTextField.text
+        newUser.password = passwordTextField.text
+        
+        
+        
+        
+        newUser.signUpInBackground { (success: Bool, error:Error?) in
+            if success {
+                print("A new user was created")
+                
+                self.presentingViewController?.dismiss(animated: true, completion: nil)
+                
+            } else {
+                
+                self.present(alertController, animated: true) {
+                    // optional code for what happens after the alert controller has finished presenting
+                }
+            }
+        }
+        
+    }
+
+    
     /*
     // MARK: - Navigation
 
