f385afee97e0538f1ca8d0e3acc55bd28a03afc3
gajeelSF <SongYuda@SFanM.local>
2017-05-27 12:22:58 -0700

    update ui

--- WhereYouAt/ProfileViewController.swift
+++ WhereYouAt/ProfileViewController.swift
@@ -31,16 +31,18 @@ class ProfileViewController: UIViewController {
         tableView.estimatedRowHeight = 120
         */
         if let profileImageFile = user?["profilePic"] as? PFFile {
+            
             profileImageFile.getDataInBackground {
                 (imageData: Data?, error: Error?) -> Void in
-                if error != nil {
+                if error == nil {
                     self.profilePic.image = UIImage(data:imageData!)
                 }
             }
-            
             let radius = profilePic.frame.width / 2
             profilePic.layer.cornerRadius = radius
             profilePic.layer.masksToBounds = true
+            
+            print("set")
         } else {
             profilePic.image = UIImage(named: "dummy_user")
             
